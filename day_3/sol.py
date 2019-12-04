737d1ccfc7e5b521a2f5d0de9c398eb4d7dd7278
PureChocolate <gurkiratsinghmard@gmail.com>
2019-12-03 22:26:53 -0800

    finished day3

--- day_3/sol.py
+++ day_3/sol.py
@@ -6,25 +6,35 @@ with open("in.txt", "r") as f:
     data = []
     data.append(a)
     data.append(b)
+    direc = {'R': (1, 0), 'D': (0,-1),'U': (0,1), 'L': (-1,0)} 
 
     intersection = []
-    been = defaultdict(dict)
+    been = defaultdict(dict)    
+    part2 = True
     
-    direc = {'R': (1, 0), 'D': (0,-1),'U': (0,1), 'L': (-1,0)}
-
     for i in range(len(data)):
+        tSteps = 0
         cord = (0,0)
         line = data[i]
         for step in line:
             move = step[0]
             num = int(step[1:])
             for n in range(num):
+                tSteps += 1
                 cord = tuple(cord[k] + direc[move][k] for k in range(len(cord)))
                 if cord not in been:
-                    been[cord] = i
+                    been[cord] = (i,tSteps)
                 else:
-                    if been[cord] is not i: intersection.append(cord)
+                    if been[cord][0] is not i:
+                        tup3 = tSteps,been[cord][1]
+                        if part2 is True: intersection.append(tup3)
+                        else: intersection.append(cord)
     small = int
+    #part 1
+    # for x in intersection:
+    #     if( (abs(0 - x[0]) + abs(0 - x[1])) < small): small = (abs(0 - x[0]) + abs(0 - x[1]))
+
+    # part 2
     for x in intersection:
-        if( (abs(0 - x[0]) + abs(0 - x[1])) < small): small = (abs(0 - x[0]) + abs(0 - x[1]))        
+        if x[0] + x[1] < small: small = x[0] + x[1]    
     print small
