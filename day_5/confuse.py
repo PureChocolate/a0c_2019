f099b0f7ee3f1170da9990292030cee4bb7c62ed
PureChocolate <gurkiratsinghmard@gmail.com>
2019-12-04 23:46:12 -0800

    idk wtf is wrong with this

--- day_5/confuse.py
+++ day_5/confuse.py
@@ -3,7 +3,7 @@ with open("in.txt", "r") as f:
 
 # print len(values)
 
-test = [3,225,1,225,6,6,1100]
+test = [1101,100,-1,4,0]
 
 def run(nums):
     i = 0
@@ -13,32 +13,45 @@ def run(nums):
         if len(pNum) < 2: pNum = ("0" * (2 - len(pNum))) + pNum
         if '1' in sNum:
             st1, st2, st3 = nums[i+1], nums[i+2], nums[i+3]
-            p1 = 0
-            p2 = 0
+            p1,p2 = 0,0
             p1 = st1 if pNum[1] is '1' else nums[st1]
             p2 = st2 if pNum[0] is '1' else nums[st2]
             nums[st3] = p1 + p2
             i += 4
         elif '2' in sNum:
             st1, st2, st3 = nums[i+1], nums[i+2], nums[i+3]
-            p1 = 0
-            p2 = 0
+            p1,p2 = 0,0
+            print "---------------"
+            # print str(nums[i]) + "is nums[i]"
+            print str(nums[i]) + "is opcode"
+            print str(sNum) + " sNUm"
+            print str(pNum[0]) + " 2nd pNUm " + str(pNum[1]) + " 1st pNUm"
+            print str(st1) + " is st1"
+            print str(nums[st1]) + " is nums[st1]"
+            print str(st2) + " is st2"
+            print str(nums[st2]) + " is nums[st2]"
             p1 = st1 if pNum[1] is '1' else nums[st1]
             p2 = st2 if pNum[0] is '1' else nums[st2]
+            print str(p1) + " is p1"
+            print str(p2) + " is p2"
+            print str(nums[st3]) + " storing at st3: " + str(st3)
             nums[st3] = p1 * p2
+            print str(nums[st3]) + " stored at st3: " + str(st3)
             i += 4
         elif '3' in sNum:
             nums[nums[i+1]] = 1
             i += 2
-        elif '4' in sNum: 
-            print str(nums[i + 1]) + "printed"
+        elif '4' in sNum:
+            #print str(nums[i + 1]) + " printed with opcode 4"
             i += 2
         elif '99' in sNum:
-            exit()
+            break
         else:
-            print "something went wrong" + str(nums[i])
-            i += 1
+            print "something wrong"
+            break
     #if nums[0] == 19690720: print(100 * nums[1] + nums[2])
+    # for i in range(len(nums)):
+    #     print(values[i] - nums[i])
 
 run(values)
 # temp = str(1002)[:-2]
