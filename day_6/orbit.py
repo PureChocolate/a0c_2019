4bc2c682e77cc88a904597b44f1e273eac9d00e5
PureChocolate <gurkiratsinghmard@gmail.com>
2019-12-06 21:16:49 -0800

    cleaned up a bit

--- day_6/orbit.py
+++ day_6/orbit.py
@@ -6,46 +6,33 @@ with open("in.txt", "r") as f:
     tot = {}
     obs['COM'] = None
     for x in values:
-        if x[0] not in tot:
+        if x[0] not in tot or x[1] not in tot:
             tot[x[0]] = 1
-        if x[1] not in tot:
             tot[x[1]] = 1
-        if x[1] not in obs:
-            obs[x[1]] = [x[0]]
-        else:
-            obs[x[0]].append(x[1])
+        if x[1] not in obs: obs[x[1]] = [x[0]]
+        else: obs[x[0]].append(x[1])
 
 # part 1 stuff
-# val = []
-# for x in tot:
-#     if x[0] not in val:
-#         val.append(x)]
+val = []
+for x in tot:
+    if x[0] not in val:
+        val.append(x)
 
 stepsSan = []
 stepsYou = []
 def run(data,key,trigger):
     c = 0
-    if trigger < 4:
-        if obs[data] is key:
-            return 0
-        else:
-            if trigger is 1: stepsYou.append(data)
-            elif trigger is 0: stepsSan.append(data)
-            c += run(obs[data][0],key,trigger) + 1
-            return c
+    if obs[data] is key or key in obs[data]:
+        return 0
     else:
-        if obs[data][0] in key: # is instead of in should have been fine but it didnt see key and data as equal???????
-            return 0
-        else:
-            if trigger is 1: stepsYou.append(data)
-            elif trigger is 0: stepsSan.append(data)
-            c += run(obs[data][0],key,trigger) + 1
-            return c
-    
-
+        if trigger is 1: stepsYou.append(data)
+        elif trigger is 0: stepsSan.append(data)
+        c += run(obs[data][0],key,trigger) + 1
+        return c
+        
+#part 2
 run('YOU',None,1)
 run('SAN',None,0)
-
 comKey = ''
 for i in stepsSan:
     if i in stepsYou:
@@ -57,10 +44,7 @@ runSan = run('SAN',comKey,4)
 print runYou + runSan
 
 # part 1
-# count = 0]
+# count = 0
 # for x in val:
-#     count += run(x)
-
+#     count += run(x,None,3)
 # print str(count) + " total direct/indect"
-
-    
